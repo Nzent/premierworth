cf4d0bb0fe59e82136a6aa2ad8d22e0b9b33b9bb
Nisal <nirvanzentinal@gmail.com>
2021-08-08 17:47:54 +0530

    init

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Premier Worth Engineering</title>
+        <meta name="description" content="Premier Worth Engineering" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section>
+       <h1>Hello</h1>
+      </section>
+    </>
+  )
+}
